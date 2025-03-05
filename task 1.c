e9687da383cd993b10e7fd6a4ce7a1fd80968593
Maruf <mdmarufsheikh707@gmail.com>
2025-03-05 23:54:00 +0600

    Add files via upload

---
+++ task 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ int a,b;
+ scanf("%d%d",&a,&b);
+ int sum=a+b;
+ int product=a*b;
+ int subtract=a-b;
+ printf("Sum=%d\nProduct=%d\nDifference=%d",sum, product, subtract);
+ return 0;
+}
+
+    
