196a304050e7f6cb5dc2832f1aeb3c670e762b8d
Maruf <mdmarufsheikh707@gmail.com>
2025-03-06 19:09:07 +0600

    Add files via upload

---
+++ task 3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+ int a,b;
+ scanf("%d%d",&a,&b); 
+ if (a>b)
+ {
+ printf("First is greater");
+ }
+ else if(b>a)
+ {
+ printf("Second is greater");
+ }
+  else
+  {
+  printf("The numbers are equal");
+  }
+ return 0;
+}
