e0c36c9cda7dcb5efd49f10bf306815c7e0d2e5c
Maruf <mdmarufsheikh707@gmail.com>
2025-03-10 14:51:29 +0600

    Add files via upload

---
+++ Task 11.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+ int s;
+ scanf("%d",&s);
+ int L=3000-(125*s*s);
+ double L2=12000/(4+(double)(s*s)/14900);
+ if (s<100)
+ {
+ printf("%d",L);
+ }
+ else if(s>=100)
+ {
+ printf("%.13lf",L2);
+ }
+ return 0;
+}
