f8abd7a945a4e543a2ce2fda0b93c43104fb0790
Maruf <mdmarufsheikh707@gmail.com>
2025-03-11 23:31:02 +0600

    Add files via upload

---
+++ L5 Task 1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int array[5];
+    for(int i=0;i<5;i++)
+    {
+        scanf("%d",&array[i]);
+        printf("[");
+        for(int j=0;j<=i;j++)
+        {
+
+            printf("%d",array [j]);
+            if(j!=i)
+            {
+                printf(",");
+            }
+
+        }
+        printf("]\n");
+    }
+    return 0;
+}
