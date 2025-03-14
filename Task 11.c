b0f9cce9f07db00c5eb7867854aeed8abdeed55d
Maruf <mdmarufsheikh707@gmail.com>
2025-03-14 20:08:57 +0600

    Update Task 11.c

--- Task 11.c
+++ Task 11.c
@@ -2,16 +2,16 @@
 int main() 
 {
  int s;
- scanf("%d",&s);
+ scanf("%d", &s);
  int L=3000-(125*s*s);
  double L2=12000/(4+(double)(s*s)/14900);
  if (s<100)
  {
- printf("%d",L);
+ printf("%d", L);
  }
  else if(s>=100)
  {
- printf("%.13lf",L2);
+ printf("%.13lf", L2);
  }
  return 0;
 }
