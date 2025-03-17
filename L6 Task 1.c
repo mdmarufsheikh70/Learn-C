817cb0d4127eedacd4324c8e2ce40ef96e867ca5
Maruf <mdmarufsheikh707@gmail.com>
2025-03-17 19:26:13 +0600

    Add files via upload

---
+++ L6 Task 1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[100],sn[100];
+    gets(s);
+    int l;
+    l= strlen(s)-1;
+
+    for(int i=l,j=0;s[i]!='\0';i--)
+    {
+       sn[j]=s[i];
+       j++;
+    }
+    printf("%s",sn);
+    return 0;
+}
+
