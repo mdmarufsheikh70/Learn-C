18bf85a1305cba6f6ddc3295e43c533193cdf00a
Maruf <mdmarufsheikh707@gmail.com>
2025-03-17 19:27:58 +0600

    Update task 1.c

--- task 1.c
+++ task 1.c
@@ -2,11 +2,11 @@
 int main()
 {
  int a,b;
- scanf("%d%d",&a,&b);
+ scanf("%d %d", &a, &b);
  int sum=a+b;
  int product=a*b;
  int subtract=a-b;
- printf("Sum=%d\nProduct=%d \n Difference=%d", sum, product, subtract);
+ printf("Sum= %d \nProduct=%d \n Difference = %d", sum, product, subtract);
  return 0;
 }
 
