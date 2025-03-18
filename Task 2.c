c0429e0ad131d88bcad5566760908866121be734
Maruf <mdmarufsheikh707@gmail.com>
2025-03-18 19:08:06 +0600

    Update Task 2.c

--- Task 2.c
+++ Task 2.c
@@ -3,10 +3,10 @@
 int main() 
 {
   float r;
-  double p= M_PI;
-  scanf("%f",&r);
-  double a=(double)p*r*r;
+  double p = M_PI;
+  scanf("%f", &r);
+  double a = (double)p* r * r;
   double c=(double)2*p*r;
-  printf("Area is %.14f\nCircumference is %.15f",a,c);
+  printf("Area is % .14f\n Circumference is %.15f", a, c);
   return 0;
 }
