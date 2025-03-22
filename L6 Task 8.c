fe64ab5ae00e2235452289ba837a120023fb5131
Maruf <mdmarufsheikh707@gmail.com>
2025-03-22 22:47:24 +0600

    Add files via upload

---
+++ L6 Task 8.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char N[50];
+    gets(N);
+    int len=0;
+    while(N[len]!='\0')
+    {
+        len++;
+    }
+    for(int i=1;i<len;i=i+2)
+    {
+        printf("%c",N[i]-32);
+    }
+   return 0;
+}
+
